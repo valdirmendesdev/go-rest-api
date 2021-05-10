7a32826e2fb66b7408eec58db86bf836c5f9a98b
Valdir Mendes <valdir.mfjesus@yahoo.com.br>
2021-05-10 17:44:32 -0300

    feat: Adding health check endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,20 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	transportHTTP "github.com/valdirmendesdev/go-rest-api/internal/transport/http"
+)
 
 // App - the struct which contains things like
 // pointers to database connections
 type App struct{}
 
-// Run - handles the startup of our application
+// Run - handles the startup of the application
 func (app *App) Run() error {
 	fmt.Println("Setting up the App")
+
+	handler := transportHTTP.NewHandler()
+	handler.SetupRoutes()
+
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		fmt.Println("Failed to set up the server")
+		return err
+	}
+
 	return nil
 }
 
-// Our main entrypoint for the application
+// Main entrypoint for the application
 func main() {
 	app := App{}
+	fmt.Println("Go REST API")
 	if err := app.Run(); err != nil {
 		fmt.Println("Error starting up the REST API")
 		fmt.Println(err)
