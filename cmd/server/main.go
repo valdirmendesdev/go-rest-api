0bcc081f50bbda542f5232a2e785fd4ba39a371e
Valdir Mendes <valdir.mfjesus@yahoo.com.br>
2021-05-10 17:25:54 -0300

    feat: Adding App type

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+// App - the struct which contains things like
+// pointers to database connections
+type App struct{}
+
+// Run - handles the startup of our application
+func (app *App) Run() error {
+	fmt.Println("Setting up the App")
+	return nil
+}
+
+// Our main entrypoint for the application
 func main() {
-	fmt.Println("Go REST API - Starting the project")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up the REST API")
+		fmt.Println(err)
+	}
 }
